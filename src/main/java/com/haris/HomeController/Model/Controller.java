b9b4f50b791b953c6be5b73abff6c6d329690cde
muhammad haris <hariskhatrijee@gmail.com>
2025-10-21 12:08:10 +0500

    updated

---
+++ src/main/java/com/haris/HomeController/Model/Controller.java
@@ -0,0 +1,18 @@
+package com.haris.HomeController.Model;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+@GetMapping ("home")
+
+    public String home (HttpServletRequest request){
+        return "This is my home and my session id: "
+                + request.getRequestedSessionId();
+}
+
+
+}
---
+++ src/main/java/com/haris/Model/Home.java
@@ -0,0 +1,4 @@
+package com.haris.Model ;
+
+public class Home {
+}
